1f1f7c20ac232bf1aea9061d4449c9120415e8ca
YoshrithMalhotra <malhotrayoshrith@gmail.com>
2025-12-09 18:40:57 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import EnquiryForm from './components/EnquiryForm';
 import EnquiryList from './components/EnquiryList';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css'; 
 import './App.css';
 
 function App() {
